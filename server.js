2c31aa5ab39c191950b636801f564695b3531d38
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2024-02-14 17:36:54 -0800

    Removed all the oauth code for this branch

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ const express = require('express');
 const https = require('https');
 const fs = require('fs');
 const path = require('path');
-const OAuthServer = require('@node-oauth/express-oauth-server');
-const Request = OAuthServer.Request;
-const Response = OAuthServer.Response;
 const bodyParser = require('body-parser'); // Required for parsing POST request body
 
 const app = express();
@@ -16,30 +13,7 @@ app.use(express.static("public"));
 
 const port = 443;
 
-//* OAUTH BOILERPLATE
 
-// app.oauth = new OAuthServer({
-//   model: require('./model') // See https://github.com/node-oauth/node-oauth2-server for specification
-// });
-
-// let request = new Request({
-//   method: 'GET',
-//   query: {},
-//   headers: { Authorization: 'Bearer foobar' }
-// });
-
-// let response = new Response({
-//   headers: {}
-// });
-
-// oauth.authenticate(request, response)
-//   .then((token) => {
-//     // The request was successfully authenticated.
-//   })
-//   .catch((err) => {
-//     // The request failed authentication.
-//   });
-//*
 
 // Serve static files from the public directory
 // app.use(express.static(path.join(__dirname, 'public')));
@@ -66,30 +40,8 @@ app.get('/', (req, res) => {
 });
 
 //get route
-app.get('/get', oauth.authenticate(), (req, res) => {
+app.get('/get', (req, res) => {
   res.render('get');
-});
-
-//post route
-app.post('oauth/token', async (req, res) => {
-  //TODO: this is where the pi will call to authenticate
-  const tokenBodyParams = new URLSearchParams();
-  tokenBodyParams.append('grant_type', 'client_credentials');
-  tokenBodyParams.append('scope', 'read');
-
-  const response = await fetch('http://localhost:8080/token', {
-    method: 'post',
-    body: tokenBodyParams,
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      'authorization': 'Basic ' + Buffer.from(`${client.id}:${client.secret}`).toString('base64'),
-    }
-  })
-  const token = await response.json()
-  const accessToken = token.access_token
-  const tokenType = token.token_type
-
-  res.render('get', {"token":token});
 });
 
 // app.post("/postToken", authenticated, (req,res) => )
