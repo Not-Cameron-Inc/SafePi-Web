2c702efb0149ab1fc4148da5301b6f72851e9d7c
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2024-02-22 13:26:14 -0800

    Added some framework code from oauth2-server code example. We now have a page with working 401 error code for posting the door

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+
+const OAuthServer = require('@node-oauth/express-oauth-server');
+const createModel = require('./model');
+const DB = require('./db');
+
 const path = require('path');
 const bodyParser = require('body-parser'); // Required for parsing POST request body
 
+//* App and HTTPS settings and usages **************/
 const app = express();
 
 app.set("view engine", "ejs");
@@ -13,8 +19,6 @@ const fetch = require('node-fetch');
 
 const port = 443;
 
-
-
 // HTTPS options
 const options = {
     key: fs.readFileSync('../keys/key.pem'),
@@ -23,10 +27,38 @@ const options = {
 
 // Create an HTTPS server
 const server = https.createServer(options, app);
+//**************************************************/
+
+//* OAUTH 2 Server Boilerplate *********************/
+const db = new DB();
+const oauth = new OAuthServer({
+  model: createModel(db)
+});
+
+db.saveClient({
+  id: process.env.CLIENT_ID,
+  secret: process.env.CLIENT_SECRET,
+  grants: ['client_credentials']
+});
+
+app.use('/token', oauth.token(), () => {
+    console.log("created token!");
+});
+//**************************************************/
 
 //root route
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+// create oauth user
+app.get('/create_user', async (req, res) => {
+    res.send("created!");
+});
+
+//signin oauth user
+app.get('/sign_in', async (req, res) => {
+    res.send("signed in!");
 });
 
 //get route
@@ -43,10 +75,10 @@ app.get("/api/getDoor", async (req, res) => {
     res.send(data);
 });
 
-app.get("/api/postDoor/:isLocked", async (req, res) => {
+app.get("/api/postDoor/:isLocked", oauth.authenticate(), async (req, res) => {
 
     var isLocked = false;
-    if(req.params.isLocked == "true"){
+    if (req.params.isLocked == "true") {
         isLocked = true;
     } else {
         isLocked = false;
@@ -62,13 +94,14 @@ app.get("/api/postDoor/:isLocked", async (req, res) => {
         },
         body: JSON.stringify({
             "fields": {
-            "isLocked": {
-                "booleanValue": isLocked
-            }
-        },
-    })});
+                "isLocked": {
+                    "booleanValue": isLocked
+                }
+            },
+        })
+    });
 
-    
+
     let data = await response.json();
     res.send(data);
 });
