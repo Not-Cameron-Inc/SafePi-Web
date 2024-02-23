24ef0784406def2bc3b436db583ac460ee9a9192
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2024-02-23 00:44:24 -0800

    Saving for the night

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+const bcrypt = require('bcrypt');
 
 const OAuthServer = require('@node-oauth/express-oauth-server');
 const createModel = require('./model');
@@ -8,6 +9,12 @@ const DB = require('./db');
 
 const path = require('path');
 const bodyParser = require('body-parser'); // Required for parsing POST request body
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //* App and HTTPS settings and usages **************/
 const app = express();
@@ -32,18 +39,19 @@ const server = https.createServer(options, app);
 //* OAUTH 2 Server Boilerplate *********************/
 const db = new DB();
 const oauth = new OAuthServer({
-  model: createModel(db)
+    model: createModel(db)
 });
 
 db.saveClient({
-  id: process.env.CLIENT_ID,
-  secret: process.env.CLIENT_SECRET,
-  grants: ['client_credentials']
+    id: process.env.CLIENT_ID,
+    secret: process.env.CLIENT_SECRET,
+    grants: ['client_credentials']
 });
 
-app.use('/token', oauth.token(), () => {
-    console.log("created token!");
-});
+
+// app.use('/token', oauth.token(), () => {
+//     console.log("created token!");
+// });
 //**************************************************/
 
 //root route
@@ -52,8 +60,35 @@ app.get('/', (req, res) => {
 });
 
 // create oauth user
-app.get('/create_user', async (req, res) => {
-    res.send("created!");
+app.post('/create_user', jsonParser, async (req, res) => {
+
+    let email = req.body.email;
+    hash = bcrypt.hashSync(`${req.body.email}:${req.body.password}`, 10);
+
+    let url = "https://firestore.googleapis.com/v1/projects/hello-world-rest-4dd02/databases/(default)/documents/user/email_auth";
+
+    let response = await fetch(url, {
+        method: 'PATCH',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "fields": {
+                "email": {
+                    "stringValue": email
+                },
+                "token": {
+                    "stringValue": hash
+                }
+            },
+        })
+    });
+
+
+    let data = await response.json();
+
+    res.send(`created user ${email} and data ${data}`);
 });
 
 //signin oauth user
@@ -75,38 +110,50 @@ app.get("/api/getDoor", async (req, res) => {
     res.send(data);
 });
 
-app.get("/api/postDoor/:isLocked", oauth.authenticate(), async (req, res) => {
+// app.get("/api/postDoor/:isLocked", oauth.authenticate(), async (req, res) => {
+app.post("/api/postDoor", async (req, res) => {
 
-    var isLocked = false;
-    if (req.params.isLocked == "true") {
-        isLocked = true;
-    } else {
-        isLocked = false;
-    }
+    const match = await bcrypt.compare(password, passwordHash);
 
-    let url = "https://firestore.googleapis.com/v1/projects/hello-world-rest-4dd02/databases/(default)/documents/door/isLockedDoc";
 
-    let response = await fetch(url, {
-        method: 'PATCH',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "fields": {
-                "isLocked": {
-                    "booleanValue": isLocked
-                }
+    //TODO: need to get email verification with get req
+    if (match) {
+        if (req.body.isLocked == "true") {
+            isLocked = true;
+        } else {
+            isLocked = false;
+        }
+
+        let url = "https://firestore.googleapis.com/v1/projects/hello-world-rest-4dd02/databases/(default)/documents/door/isLockedDoc";
+
+        let response = await fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-        })
-    });
+            body: JSON.stringify({
+                "fields": {
+                    "isLocked": {
+                        "booleanValue": isLocked
+                    }
+                },
+            })
+        });
 
 
-    let data = await response.json();
-    res.send(data);
+        let data = await response.json();
+        res.send(data);
+    }
+    else {
+        res.send("Wrong Credentials!");
+    }
 });
 
-// app.post("/postToken", authenticated, (req,res) => )
+app.post("/api/email_auth", async (req, res) => {
+
+    res.send("Not ready yet.");
+});
 
 // Start the HTTPS server
 server.listen(port, () => {
